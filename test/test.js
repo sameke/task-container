b8902ef69218b4d02baf129b3a18626426e8197b
Ben Johnson <bjohnson@colsa.com>
2017-03-03 12:30:25 -0600

    added ability to kill all handlers

--- test/test.js
+++ test/test.js
@@ -1,16 +1,23 @@
 const TaskContainer = require('../lib/TaskContainer');
 
 let p = new TaskContainer({maxHandlers: 10, maxCallsPerHandler: 10});
+let count = 0;
 p.on('message', (msg) => {
         console.log('ignored message received:');
         console.log(msg);
     });
 
 setInterval(() => {
-    
+      
     p.run(require.resolve('./testChild'), {test: 'hello world'}, (err, res) => {
         console.log(res);
         //console.log(err);
     });
-}, 5000);
+    count++;
+
+    if(count >= 10) {
+        p.killAll();
+        count = 0;
+    }    
+}, 1000);
 
--- test/testChild.js
+++ test/testChild.js
@@ -3,5 +3,5 @@ module.exports = (options, callback) => {
         process.send({isLog: true, data: 'this is an ignored message from the task'});
         callback(null, options);
         //callback(new Error('test error'));
-    }, 1000);    
+    }, 3000);    
 }
